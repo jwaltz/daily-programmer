d11b83498321b6e60cb6a1530b89d45f1704bdc5
Jonathan Waltz <jon@waltzhome.com>
2013-07-24 16:43:43 +0000

    Added Visitor class and implemented incoming visitors to rooms

---
+++ 130_foot_traffic_generator/Visitor.java
@@ -0,0 +1,29 @@
+public class Visitor {
+    private boolean visiting;
+    private int enterTime;
+    private int exitTime;
+    private int visitorNum;
+
+    public Visitor(int num, int enterTime) {
+        this.visiting = true;
+        this.enterTime = enterTime;
+        this.exitTime = 0;
+        this.visitorNum = num;        
+    }
+
+    public int getVisitDuration() {
+        return this.exitTime - this.enterTime;
+    }
+
+    public boolean isVisiting() {
+        return this.visiting;
+    }
+    
+    public void setVisiting(boolean visitingStatus) {
+        this.visiting = visitingStatus;
+    }
+
+    public int getEnterTime() {
+        return this.enterTime;
+    }
+}
