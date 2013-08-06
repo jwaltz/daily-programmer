b97941d722c6f82ee52faafbba731caa30781543
Jonathan Waltz <jon@waltzhome.com>
2013-08-06 19:28:16 +0000

    Added output for no solution

--- 134E_n_divisible_digits/NDivisibleDigits.java
+++ 134E_n_divisible_digits/NDivisibleDigits.java
@@ -6,7 +6,8 @@ public class NDivisibleDigits {
         }
         int n = Integer.parseInt(args[0]);
         int m = Integer.parseInt(args[1]);
-        System.out.println(getAnswer(n, m));
+        int answer = getAnswer(n, m);
+        System.out.println(answer == 0 ? "No solution." : answer);
     }
     private static int getAnswer(int n, int m) {
         int answer = (int)Math.pow(10, n) - 1;
